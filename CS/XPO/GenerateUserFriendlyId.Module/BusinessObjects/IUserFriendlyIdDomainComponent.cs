353ae4435f52adf1f0121c602268c944f666d304
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-28 18:00:14 +0400

    remove old xpo solution

--- CS/XPO/GenerateUserFriendlyId.Module/BusinessObjects/IUserFriendlyIdDomainComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using DevExpress.ExpressApp.DC;
-using DevExpress.Persistent.Base;
-using DevExpress.Persistent.Validation;
-using GenerateUserFriendlyId.Module;
-
-namespace GenerateUserFriendlyId.Module.BusinessObjects {
-     [DomainComponent]
-    public interface IUserFriendlyIdDomainComponent : ISupportSequentialNumber { }
-}
