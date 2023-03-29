25c9d6f46692ab147f392cff4ea1cf38e1020c1a
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-29 16:50:23 +0400

    simplify

--- CS/XPO/SequenceGenerator/SequenceGenerator.Module/BusinessObjects/Contact.cs
+++ CS/XPO/SequenceGenerator/SequenceGenerator.Module/BusinessObjects/Contact.cs
@@ -18,9 +18,7 @@ namespace Demo.Module.BusinessObjects {
 
         string firstName;
         string lastName;
-        Sex sex;
-        int age;
-        Address address;
+  
         public string FirstName {
             get { return firstName; }
             set { SetPropertyValue("FirstName", ref firstName, value); }
@@ -29,27 +27,7 @@ namespace Demo.Module.BusinessObjects {
             get { return lastName; }
             set { SetPropertyValue("LastName", ref lastName, value); }
         }
-        public int Age {
-            get { return age; }
-            set { SetPropertyValue("Age", ref age, value); }
-        }
-        public Sex Sex {
-            get { return sex; }
-            set { SetPropertyValue("Sex", ref sex, value); }
-        }
-        [Association]
-        public Address Address {
-            get { return address; }
-            set { SetPropertyValue("Address", ref address, value); }
-        }
-        [PersistentAlias("concat(FirstName, LastName)")]
-        public string FullName {
-            get { return ObjectFormatter.Format("{FirstName} {LastName}", this, EmptyEntriesMode.RemoveDelimiterWhenEntryIsEmpty); }
-        }
         public Contact(Session session) : base(session) { }
     }
-    public enum Sex {
-        Male,
-        Female
-    }
+ 
 }
